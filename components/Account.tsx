c4fb6e46a2db01c86fb50ae5d9023ba211648847
GyureszBa <gyorgy.dezsenyi@gmail.com>
2022-09-23 13:33:42 +0200

    reac typescript fixes

--- components/Account.tsx
+++ components/Account.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, View, Alert, Text, TextInput} from 'react-native'
 import { Button, Input } from 'react-native-elements'
 import { Session } from '@supabase/supabase-js'
 import Select, { SelectItem } from '@redmin_delishaj/react-native-select'
+import React from 'react'
 
 export default function Account({ session }: { session: Session }) {
   const [loading, setLoading] = useState(true)
