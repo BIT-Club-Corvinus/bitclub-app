80c83200289563b144c00e0c3f296cd6b9bf6195
GyureszBa <gyorgy.dezsenyi@gmail.com>
2022-09-28 13:28:29 +0200

    Update Auth.tsx

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Alert, StyleSheet, View } from 'react-native'
+import { Alert, Pressable, StyleSheet, View, Text } from 'react-native'
 import { supabase } from '../lib/supabase'
 import { Button, Input } from 'react-native-elements'
 
@@ -31,31 +31,33 @@ export default function Auth() {
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <View style={[styles.verticallySpaced, styles.mt20]}>
         <Input
-                  label="Email"
-                  leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-                  onChangeText={(text) => setEmail(text)}
-                  value={email}
-                  placeholder="email@address.com"
-                  autoCapitalize={'none'} autoCompleteType={undefined}        />
+          label="Email"
+          leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          placeholder="email@address.com"
+          autoCapitalize={'none'} autoCompleteType={undefined} />
       </View>
       <View style={styles.verticallySpaced}>
         <Input
-                  label="Password"
-                  leftIcon={{ type: 'font-awesome', name: 'lock' }}
-                  onChangeText={(text) => setPassword(text)}
-                  value={password}
-                  secureTextEntry={true}
-                  placeholder="Password"
-                  autoCapitalize={'none'} autoCompleteType={undefined}        />
+          label="Password"
+          leftIcon={{ type: 'font-awesome', name: 'lock' }}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry={true}
+          placeholder="Password"
+          autoCapitalize={'none'} autoCompleteType={undefined} />
       </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button title="Sign in" disabled={loading} onPress={() => signInWithEmail()} />
+      <View style={[styles.mt20, styles.button]}>
+        <Pressable disabled={loading} onPress={() => signInWithEmail()}>
+          <Text style={styles.buttonText}>Bejelentkezés</Text>
+        </Pressable>
       </View>
-      <View style={styles.verticallySpaced}>
-        <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
+      <View style={styles.mt20}>
+        <Text style={styles.registrationText}>Még nincs fiókod? Regisztrálj itt!</Text>
       </View>
     </View>
   )
@@ -65,13 +67,33 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 40,
     padding: 12,
+    alignItems: 'center'
   },
   verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
+    paddingTop: '4%',
+    paddingBottom: '4%',
     alignSelf: 'stretch',
   },
   mt20: {
     marginTop: 20,
   },
+  button: {
+    backgroundColor: "#12b0b0",
+    width: '85%',
+    padding: '3%',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  registrationText: {
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    textDecorationColor: '#12b0b0',
+    textDecorationStyle: 'solid',
+    color: '#12b0b0'
+  },
+  buttonText:{
+    color: 'white',
+    fontSize: 20
+  }
 })
