d6f4d23cd2d33f5a36524cc25c8439995bf4221e
Péter Palotai <ppalotai4@gmail.com>
2023-03-30 16:04:34 +0200

    hotfixes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabase'
-import Auth from './components/Login'
-import Account from './components/Account'
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { View } from 'react-native'
 import { Session } from '@supabase/supabase-js'
 import 'react-native-url-polyfill/auto'
-import React from 'react'
 import Home from './components/Home'
 import Main from './components/Main'
+import { supabase } from './lib/supabase';
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null)
